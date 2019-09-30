e489a8a35a6546f7cf38ff71c0b64e0ddd78346b
Dedy Prasetyo Hermawan <dedy.prasetyo.h@gmail.com>
2019-09-30 21:47:03 +0700

    debuging & refactoring main.go,

--- 0. Count/main.go
+++ 0. Count/main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
 func main() {
-	var input string
-	reader := bufio.NewReader(os.Stdin)
-
-	fmt.Println("Input kata/kalimat yang ingin di cek huruf hidup/mati : ")
-	input, _ = reader.ReadString('\n')
-
-	countVowel, countConsonant := getCount(input)
-	fmt.Printf("huruf mati: %v, huruf hidup: %v", countVowel, countConsonant)
+	countVowel, countConsonant := getCount("omama")
+	fmt.Printf("huruf hiduo: %d, huruf mati: %d", countVowel, countConsonant)
 }
