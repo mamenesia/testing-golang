46096a408f0762081510c536ba3142646fcba462
Dedy Prasetyo Hermawan <dedy.prasetyo.h@gmail.com>
2019-09-30 13:07:41 +0700

    refactoring untuk mendapatkan user input

--- 0. Count/main.go
+++ 0. Count/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	countVowel, countConsonant := getCount("omama")
+	var input string
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Input kata/kalimat yang ingin di cek huruf hidup/mati : ")
+	input, _ = reader.ReadString('\n')
+
+	countVowel, countConsonant := getCount(input)
 	fmt.Printf("huruf mati: %v, huruf hidup: %v", countVowel, countConsonant)
 }
